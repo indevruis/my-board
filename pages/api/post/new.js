da63f40681eb062e531cf79b1456c91763725c28
ruirun <indevruis@gmail.com>
2023-05-20 10:19:32 +0900

    [Add] author 디비에 추가

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,4 +1,6 @@
 import { connectDB } from "@/util/database";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(req, res) {
   const date = new Date();
@@ -7,21 +9,29 @@ export default async function handler(req, res) {
     month: date.getMonth() + 1,
     day: date.getDate(),
   };
-  if (req.method == "POST") {
-    if (req.body.title === "" || req.body.writer === "") {
-      res.status(500).json("제목을 작성하세요.");
+
+  const session = await getServerSession(req, res, authOptions);
+  
+  if (session) {
+    if (req.method == "POST") {
+      if (req.body.title === "" || req.body.writer === "") {
+        res.status(422).json("제목을 작성하세요.");
+      }
+      try {
+        const db = (await connectDB).db("my-board");
+        const result = await db.collection("post").insertOne({
+          title: req.body.title,
+          content: req.body.content,
+          writer: req.body.writer,
+          date: `${today.year}/${today.month}/${today.day}`,
+          author: session.user.email,
+        });
+        res.redirect(302, "/board");
+      } catch (error) {
+        res.status(500).json("실패");
+      }
     }
-    try {
-      const db = (await connectDB).db("my-board");
-      const result = await db.collection("post").insertOne({
-        title: req.body.title,
-        content: req.body.content,
-        writer: req.body.writer,
-        date: `${today.year}/${today.month}/${today.day}`,
-      });
-      res.redirect(302, "/board");
-    } catch (error) {
-      res.status(500).json("실패");
-    }
+  } else {
+    res.status(422).json("로그인이 필요합니다.");
   }
 }
